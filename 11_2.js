4091a72c469d8a298ca453e0bef014b9ff831b7d
Raimei <loonysnow@gmail.com>
2020-10-16 02:06:45 +0500

    change 11_2

--- 11_2.js
+++ 11_2.js
@@ -1,9 +1,15 @@
-let arr = [4, 8, null, 16, 23, 'IDDQD', 42];
+let arr = [4, 8, null, 16, 23, 'IDDQD', 42, 0];
 
-function checkArray(array, even, odd){
+function checkArray(array){
+  let even = 0;
+  let odd = 0;
+  let zero = 0;
+
   for (let i = 1; i <= array.length; i++) {
-    if (typeof array[i] == 'number' && !isNaN(array[i])){
-      if (array[i] % 2 == 0){
+    if (typeof arr[i] == 'number' && !isNaN(arr[i])){
+      if (arr[i] === 0) {
+        zero++;
+      } else if (arr[i] % 2 == 0){
         even++;
       } else {
         odd++;
@@ -13,7 +19,7 @@ function checkArray(array, even, odd){
 
   console.log(`Чётных чисел: ${even}`);
   console.log(`Нечётных чисел: ${odd}`);
-  console.log(`Значение нулевого элемента: ${arr[0]}`);
+  console.log(`Нулей: ${zero}`);
 }
 
-checkArray(arr, 0, 0);
+checkArray(arr);
